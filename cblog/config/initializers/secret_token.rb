29175f25c7a0751b1be0261440970b7afc15ae9e
cblop <commanderblop@gmail.com>
2012-06-18 10:14:49 +0100

    added rails scaffolding

---
+++ cblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cblog::Application.config.secret_token = 'ea5eb28d1c12fc938d8344a41c66637baf41f9bc8c3bae4cae36e1852118fe2b44b2c89856f19e0f296e7ec6dfad0e041993a9f7dddb64ab4465528b6cacb10f'
